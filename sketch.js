d3b17eb6b6fc0692516b0433e7d00087c781ce07
raghavthecoder <65603739+raghavthecoder@users.noreply.github.com>
2020-07-04 15:59:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var ball,database,position;
+
+function setup(){
+    database=firebase.database();
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var ballpos=database.ref('ball/position');
+    ballpos.on("value",readposition);
+}
+
+function draw(){
+    background("white");
+    if(position!==undefined){
+        if(keyDown(LEFT_ARROW)){
+            //changePosition(-1,0);
+            writeposition(-1,0);
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            //changePosition(1,0);
+            writeposition(1,0);
+        }
+        else if(keyDown(UP_ARROW)){
+            //changePosition(0,-1);
+            writeposition(0,-1);
+        }
+        else if(keyDown(DOWN_ARROW)){
+            //changePosition(0,+1);
+            writeposition(0,+1);
+        }
+     }
+
+    
+    drawSprites();
+}
+
+function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
+
+function readposition(data){
+   position=data.val();
+   ball.x=position.x;
+   ball.y=position.y;
+}
+
+function writeposition(x,y){
+   database.ref('ball/position').set({
+       x: position.x+x,
+       y: position.y+y
+   })
+}
+
+
